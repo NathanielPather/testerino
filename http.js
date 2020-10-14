48ede37b2e81dc430b36677daeb453797ce73dc3
Nathaniel Pather <nathaniel_pather@hotmail.com>
2020-10-14 19:25:14 +1000

    first commit

---
+++ http.js
@@ -0,0 +1,30 @@
+// http://localhost:3000/
+
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+// creates a server
+const server = http.createServer((req, res) => {
+  // that reads the request
+  //              \/
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+  // and reads this one too
+  //               \/
+  if (req.url === "/test") {
+    fs.readFile(path.join(__dirname, "test.html"), (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+// listens to \/ port and logs if the site is running
+server.listen(3000, () => console.log("LocalSite Running"));
